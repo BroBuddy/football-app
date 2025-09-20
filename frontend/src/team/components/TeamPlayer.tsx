0e7705f7c2e32a6fbc8204a875ae10c3cb75c33e
Buddy Stone <buddy.cgn@gmail.com>
2025-09-20 11:21:21 +0200

    added positions

--- frontend/src/team/components/TeamPlayer.tsx
+++ frontend/src/team/components/TeamPlayer.tsx
@@ -3,7 +3,7 @@ import { Player } from '@/player/types/Player';
 import { formatMarketValue } from '@/utils';
 import Image from '@/ui/Image';
 import { Link } from 'react-router-dom';
-import { Calendar } from 'lucide-react';
+import { Calendar, MapPin } from 'lucide-react';
 import OverallRating from '@/ui/OverallRating';
 import { PlayerDetail } from '@/overview/services/types/BestPlayers';
 
@@ -18,8 +18,11 @@ const TeamPlayer: React.FC<TeamPlayerProps> = ({ player }) => {
       to={`/players/${player.id}`} 
       className="flex justify-between items-center bg-grey-900 rounded cursor p-2 m-b-1">
       <div className='flex items-center'>
-        <Image src={player.nation.flagUrl} alt={player.nation.name} className="w-3" />
-        <div className="text-sm text-white p-l-4">
+        <OverallRating rating={player.overallRating} />
+
+        <Image src={player.nation.flagUrl} alt={player.nation.name} className="w-3 m-x-4" />
+        
+        <div className="text-sm text-white">
             {player.firstName}{' '}
             {player.lastName}
         </div>
@@ -27,9 +30,11 @@ const TeamPlayer: React.FC<TeamPlayerProps> = ({ player }) => {
       
       <div className='flex flex-row items-center gap-3'>
         <div className="flex items-center text-left w-10 text-grey-200">
-            <OverallRating rating={player.overallRating} />
+            <MapPin size={15} className="m-r-1" />
+            <span className='text-sm'>
+                {player.mainPositions.toLocaleUpperCase()}
+            </span>
         </div>
-
         <div className="flex items-center text-left w-10 text-grey-200">
             <Calendar size={15} className="m-r-1" />
             <span className='text-sm'>
